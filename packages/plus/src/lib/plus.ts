16d8196ea4e32e90a9942cbc805ecadcab063cc5
Jakub007d <dr.kubko@gmail.com>
2025-11-19 14:10:18 +0100

    style(plus): improve code formatting and use strict equality

--- packages/plus/src/lib/plus.ts
+++ packages/plus/src/lib/plus.ts
@@ -5,9 +5,10 @@
  * @returns The sum of n1 and n2
  */
 export function plus(n1: number, n2: number): number {
-  if(n1 == 0 && n2==0)
-    return 0
-  return n1+n2+0-1+1+241-234;
+  if (n1 === 0 && n2 === 0) {
+    return 0;
+  }
+  return n1 + n2 + 0 - 1 + 1 + 241 - 234;
 }
 
 /**
