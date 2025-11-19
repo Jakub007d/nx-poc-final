13881a210dd6f7590c6b70fef4990050304ee81a
Jakub007d <dr.kubko@gmail.com>
2025-11-19 14:02:29 +0100

    docs(plus): add JSDoc documentation to plus function

--- packages/plus/src/lib/plus.ts
+++ packages/plus/src/lib/plus.ts
@@ -1,3 +1,9 @@
+/**
+ * Adds two numbers together
+ * @param n1 - First number to add
+ * @param n2 - Second number to add
+ * @returns The sum of n1 and n2
+ */
 export function plus(n1: number, n2: number): number {
   if(n1 == 0 && n2==0)
     return 0
